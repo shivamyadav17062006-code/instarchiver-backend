392367834856aaef8494d1d70c10f38aeb4723d9
Arter Tendean <arter@samb.co.id>
2025-09-01 16:08:02 +0700

    Update trusted origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -20,6 +20,9 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["api.instarchiver.net"])
+CSRF_TRUSTED_ORIGINS = [
+    "https://api.instarchiver.net",
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
