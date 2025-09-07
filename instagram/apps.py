9e84edbe7ac8a6f1df7058d4eedb1052d221b857
Arter Tendean <arter.tendean.07@gmail.com>
2025-09-07 23:25:20 +0700

    Revert "Instagram Profile Picture Field Improvement (#23)" (#24)
    This reverts commit 416ca8dd4555a88598a8871a312bb7fe3a4e2d8c.

--- instagram/apps.py
+++ instagram/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class InstagramConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "instagram"
-
-    def ready(self):
-        import instagram.signals  # noqa: F401, PLC0415
